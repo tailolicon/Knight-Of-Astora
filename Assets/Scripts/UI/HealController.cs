0d60c3c0d813b2899c5a2fdd4b53153ddea5329d
j4stVV <quachviet03@gmail.com>
2024-11-07 16:37:08 +0700

    Health and Mana UI

---
+++ Assets/Scripts/UI/HealController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealController : MonoBehaviour
+{
+    public Slider slider;
+
+    public void SetMaxHealth(int maxHealth)
+    {
+        slider.maxValue = maxHealth;
+        slider.value = maxHealth;
+    }
+
+    public void SetHealth(int health)
+    {
+        slider.value = health;  
+    }
+    
+}
