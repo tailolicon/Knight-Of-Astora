92b66fbd0619bfe71dc04d4e363e9042274dca98
j4stVV <quachviet03@gmail.com>
2024-11-08 16:35:12 +0700

    Add Player State

---
+++ Assets/Scripts/PlayerState.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerState : MonoBehaviour
+{
+    public bool isOnGround;
+    public bool isRolling;
+    public bool isDashing = false;
+    public bool isJumping = false;
+    public bool recoilingX = false;
+    public bool lookingRight;
+
+    public bool isHealing;
+
+    public bool invincible = false;
+}
